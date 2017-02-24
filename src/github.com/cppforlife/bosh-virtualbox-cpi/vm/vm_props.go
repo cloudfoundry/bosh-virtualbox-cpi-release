dae4830166972201562798cb251c3e5de34ff126
Hunan Rostomyan <hunan131@gmail.com>
2017-02-23 23:59:27 -0800

    Add shared folders to cloud properties

--- src/github.com/cppforlife/bosh-virtualbox-cpi/vm/vm_props.go
+++ src/github.com/cppforlife/bosh-virtualbox-cpi/vm/vm_props.go
@@ -1,8 +1,14 @@
 package vm
 
 import (
+	"errors"
+
 	apiv1 "github.com/cppforlife/bosh-cpi-go/apiv1"
 )
+
+type SharedFolder struct {
+	HostPath string `json:"host_path"`
+}
 
 type VMProps struct {
 	Memory        int
@@ -11,6 +17,8 @@ type VMProps struct {
 
 	GUI              bool
 	ParavirtProvider string `json:"paravirtprovider"`
+
+	SharedFolders []SharedFolder `json:"shared_folders"`
 }
 
 func NewVMProps(props apiv1.VMCloudProps) (VMProps, error) {
@@ -25,6 +33,12 @@ func NewVMProps(props apiv1.VMCloudProps) (VMProps, error) {
 	err := props.As(&vmProps)
 	if err != nil {
 		return VMProps{}, err
+	}
+
+	for _, folder := range vmProps.SharedFolders {
+		if folder.HostPath == "" {
+			return VMProps{}, errors.New("Expected host paths not to be empty")
+		}
 	}
 
 	return vmProps, nil
