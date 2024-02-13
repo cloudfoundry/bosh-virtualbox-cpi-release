ad2081b0995bc531260af442f19a2c5c41dd1b17
Benjamin Gandon <bgandon@users.noreply.github.com>
2024-02-13 01:50:18 +0100

    Migrate to ginkgo v2

--- src/bosh-virtualbox-cpi/driver/ssh_runner_test.go
+++ src/bosh-virtualbox-cpi/driver/ssh_runner_test.go
@@ -4,7 +4,7 @@ import (
 	"os"
 
 	boshlog "github.com/cloudfoundry/bosh-utils/logger"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	. "bosh-virtualbox-cpi/driver"
